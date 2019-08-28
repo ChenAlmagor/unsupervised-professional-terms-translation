f54bd368ff87bbdd521109f1d2918631a7702760
Chen Almagor <chenalmagor@gmail.com>
2019-08-28 18:34:16 +0300

    nn

--- neural-networks/eval_for_medical.py
+++ neural-networks/eval_for_medical.py
@@ -7,8 +7,8 @@ src_id2word, src_word2id, src_embeddings = utils.read_txt_embeddings('data/wiki.
 tgt_id2word, tgt_word2id, tgt_embeddings = utils.read_txt_embeddings('data/wiki.%s.vec' % params.tgt_lang, params.n_eval_ex, False)
 
 print("%s_%s" % (params.src_lang, params.tgt_lang))
-T = np.load("output/en_es_T-medical.npy")
-TranslatedX = src_embeddings.dot(np.transpose(T))
+
+TranslatedX  = np.load("output/TranslatedX-relu.npy")
 
 cross_dict = utils.load_dictionary('../data-test/medical/test-dict-only-words.txt', src_word2id, tgt_word2id)
 utils.get_word_translation_accuracy(params.src_lang, src_word2id, TranslatedX,
